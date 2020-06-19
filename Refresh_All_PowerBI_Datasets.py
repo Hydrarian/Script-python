11a4b943d7fef5426fa5a3d97693f487913a086c
Marco Tommolini <tommolini@gberardi.com>
2020-06-19 12:25:40 +0200

    Aggiunto sleep tra gruppi e escluso dataset Venduto 2019

--- Refresh_All_PowerBI_Datasets.py
+++ Refresh_All_PowerBI_Datasets.py
@@ -2,19 +2,15 @@ import adal
 import requests
 import json
 import pandas
+import time
 
 authority_url = 'https://login.windows.net/common'
 resource_url = 'https://analysis.windows.net/powerbi/api'
 client_id = '2ac99ccf-962b-43e3-b6a0-84c439aa45c6'
 username = 'pbiadmin@gberardi.com'
 password = 'BI2020!!!'
-context = adal.AuthenticationContext(authority=authority_url,
-                                     validate_authority=True,
-                                     api_version=None)
-token = context.acquire_token_with_username_password(resource=resource_url,
-                                                     client_id=client_id,
-                                                     username=username,
-                                                     password=password)
+context = adal.AuthenticationContext(authority=authority_url, validate_authority=True, api_version=None)
+token = context.acquire_token_with_username_password(resource=resource_url, client_id=client_id, username=username, password=password)
 
 access_token = token.get('accessToken')
 
@@ -31,7 +27,8 @@ for group in groups:
     datasets_request = json.loads(requests.get(url=datasets_request_url, headers=header).content)
     datasets = [d['id'] for d in datasets_request['value']]
     for dataset in datasets:
-        refresh_url = 'https://api.powerbi.com/v1.0/myorg/groups/' + group + '/datasets/' + dataset + '/refreshes'
-        r = requests.post(url=refresh_url, headers=header)
-
+        if dataset!='9ea08f66-4f79-4b88-a236-e5c03e914d50': #escludo il report del 2019
+            refresh_url = 'https://api.powerbi.com/v1.0/myorg/groups/' + group + '/datasets/' + dataset + '/refreshes'
+            r = requests.post(url=refresh_url, headers=header)
+    time.sleep(420) #attendo 7 minuti prima di lanciare un altro group (area)
 final = pandas.DataFrame()
